432c9d66235a509bac64ae74475f60e86754a64f
Vishal Karthikeyan P <vishalkarthikeyanp@gmail.com>
2026-07-07 05:55:56 +0000

    stubs for further programming

--- Employee.java
+++ Employee.java
@@ -10,18 +10,36 @@ class employee{
         this.department = department;
         this.salary = salary;
     }
+}
 
+public class Employee{
+    static ArrayList<employee> eList=new ArrayList<>();
+    static Scanner sc=new Scanner(System.in);
+    public static void add(){
+
+    }
+    public static void search()
+    {
+
+    }
+    public static void delete()
+    {
+
+    }
+    public static void update()
+    {
+
+    }
+    public static void disp()
+    {
+        
+    }
     //add()
     //search()
     //delete()
     //update()
-
-}
-
-public class Employee{
-    
     public static void main(String[] args){
-        ArrayList<employee> eList=new ArrayList<>();
+        
 
     }
 }
