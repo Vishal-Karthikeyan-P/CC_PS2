590531e05cbafe0e5d6261bd369287c57681b1f5
Vishal Karthikeyan P <vishalkarthikeyanp@gmail.com>
2026-07-07 06:17:21 +0000

    Update function completed

--- Employee.java
+++ Employee.java
@@ -16,7 +16,7 @@ public class Employee{
     static ArrayList<employee> eList=new ArrayList<>();
     static Scanner sc=new Scanner(System.in);
     public static void add(){
-
+        
     }
     public static void search()
     {
@@ -26,13 +26,35 @@ public class Employee{
     {
 
     }
+    //This function updates all the values of a particular employee ID and updates it in the same ID.
     public static void update()
     {
-
+        System.out.println("Enter emp id to update:");
+        int update=sc.nextInt();
+        int idx=0;
+        sc.nextLine();
+        String nName;
+        String nDept;
+        String nSalary;
+        for (employee e : eList)
+        {
+            if(e.empID == update)
+            {
+                System.out.println("Enter new name:");
+                nName=sc.nextLine();
+                System.out.println("Enter new department:");
+                nDept=sc.nextLine();
+                System.out.println("Enter new Salary:");
+                nSalary=sc.nextLine();
+                eList.set(idx, new employee(e.empID, nName, nDept, nSalary));
+                break;
+            }
+            idx++;
+        }
     }
     public static void disp()
     {
-        
+
     }
     //add()
     //search()
